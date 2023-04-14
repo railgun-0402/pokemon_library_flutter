2a95c99b3f900b89028bb144c1aef68c615b746f
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-04-14 20:45:20 +0900

    ページの追加
    ・タブの作成
    ・設定ページの作成

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,18 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_library_flutter/view/setting_top/setting_top_page.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+import 'home_strings.dart';
+
+final _pageWidgets = [const HomePageBase(), const SettingTopPage()];
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  /// タブの高さ
+  static const double tabBarHeight = 10;
+
+  static const tabs = [
+    _TabWidget(tabTitle: homeTabTitle),
+    _TabWidget(tabTitle: settingTabTitle),
+  ];
+
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: Center(
-        child: Image.network(
-            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
-            height: 200,
-            width: 150),
-      )),
+        body: _pageWidgets.elementAt(_currentIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          fixedColor: Colors.blueAccent,
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), label: homeTabTitle),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: settingTabTitle)
+          ],
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) => setState(() => _currentIndex = index);
+}
+
+///
+/// Homeのページベース
+///
+class HomePageBase extends StatelessWidget {
+  const HomePageBase({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+                height: 100,
+                width: 100),
+            const Text(
+              'pikachu',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+///
+/// タブの作成
+///
+class _TabWidget extends StatelessWidget {
+  const _TabWidget({Key? key, required this.tabTitle}) : super(key: key);
+
+  /// タブタイトル
+  final String tabTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Align(alignment: Alignment.topCenter, child: Text(tabTitle)),
+      ),
     );
   }
 }
---
+++ lib/view/home_strings.dart
@@ -0,0 +1,8 @@
+/*
+ * home_pageで使用する文言をここで管理する
+ */
+/// ホームタブタイトル
+const String homeTabTitle = 'ホーム';
+
+/// 設定タブタイトル
+const String settingTabTitle = '設定';
